130367ec8091604a4d583476dae5c2a190ae249e
김성욱 <70447614+Kimsw5368@users.noreply.github.com>
2021-03-28 18:52:26 +0900

    spring mvc

---
+++ springmvc/src/main/java/com/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,48 @@
+package com.hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class MappingController {
+
+    @GetMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "OK";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable String userId) {
+        log.info("mappingPath userId = {}", userId);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable String orderId){
+        log.info("mappingPath userId = {}, orderId = {}", userId, orderId);
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
